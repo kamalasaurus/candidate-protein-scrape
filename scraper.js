5612a8a73ebf331a8800e0af73384006a5d7a291
kamalasaurus <kamalasaurus@gmail.com>
2024-07-18 17:15:19 -0400

    typo

--- scraper.js
+++ scraper.js
@@ -28,36 +28,34 @@ export default async function scraper() {
   const gene_to_protein_list = Object.fromEntries(gene_to_protein)
 
   for (const gene of Object.keys(gene_to_protein_list)) {
-    for (const protein_list of gene_to_protein_list[gene]) {
-      let proteins = []
-      for (const protein of protein_list.split(',')) {
-        try {
-          const url = `https://rest.uniprot.org/uniprotkb/${protein}`;
-          const response = await fetch(url);
-          const data = await response.json();
+    let proteins = []
+    for (const protein of gene_to_protein_list[gene]) {
+      try {
+        const url = `https://rest.uniprot.org/uniprotkb/${protein}`;
+        const response = await fetch(url);
+        const data = await response.json();
 
-          const sequence = data.sequence.value
-          const length = data.sequence.length
-          const score = data.annotationScore
+        const sequence = data.sequence.value
+        const length = data.sequence.length
+        const score = data.annotationScore
 
-          proteins.push({protein, sequence, length, score})
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
+        proteins.push({protein, sequence, length, score})
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      const exemplar = proteins.sort((a, b) => b.score - a.score)[0]
-      const {protein, sequence, length, score} = exemplar
-      const header = `>${gene} ${protein} ${length} ${score}`
-      await logger.write(encoder.encode(`${header}\n${sequence}\n`))
-      console.log(header, sequence)
+    }
+    const exemplar = proteins.sort((a, b) => b.score - a.score)[0]
+    const {protein, sequence, length, score} = exemplar
+    const header = `>${gene} ${protein} ${length} ${score}`
+    await logger.write(encoder.encode(`${header}\n${sequence}\n`))
+    console.log(header, sequence)
 
-      const multiple_exemplars = proteins.filter(p => p.score === exemplar.score)
+    const multiple_exemplars = proteins.filter(p => p.score === exemplar.score)
 
-      if (multiple_exemplars.length > 1) {
-        const row = `${gene}\t${exemplar.score}\t${multiple_exemplars.map(p => p.protein).join(',')}\t${multiple_exemplars.map(p => p.sequence).join(',')}`
-        await multiples_logger.write(encoder.encode(`${row}\n`))
-        console.log(row)
-      }
+    if (multiple_exemplars.length > 1) {
+      const row = `${gene}\t${exemplar.score}\t${multiple_exemplars.map(p => p.protein).join(',')}\t${multiple_exemplars.map(p => p.sequence).join(',')}`
+      await multiples_logger.write(encoder.encode(`${row}\n`))
+      console.log(row)
     }
   }
 
